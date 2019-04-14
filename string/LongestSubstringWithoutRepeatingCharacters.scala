a0f40322626ff9c9d896b95d2eac028cd6ec901e
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2019-04-14 15:37:07 +0300

    Enhance algoritm to find duplicate chars in two ways

--- string/LongestSubstringWithoutRepeatingCharacters.scala
+++ string/LongestSubstringWithoutRepeatingCharacters.scala
@@ -1,3 +1,4 @@
+import scala.util.control.Breaks._
 object Solution {
 
     def main(args: Array[String]): Unit = {
@@ -14,13 +15,40 @@ object Solution {
 
         for (i <- 0 until s.length) {
             println(" # " + i + " is "+ s(i))
-            // check uniquest in some seq from `start` to `i`
-            for (j <- start until i) {
-                if (s(j) == s(i)) {
-                    println("        dup found: " + s(j))
-                    result = if (result > i - start) result else i - start
-                    start = i
+            breakable {
+
+                var startIndex = 0
+                var endIndex = 0
+
+                for (j <- i + 1 until s.length) {
+                    println(s"        $i,   $j")
+                    println("        " + s(i) + " == " + s(j))
+
+                    if (s(j) == s(i)) {
+                        println("        dup found: " + s(j))
+                        endIndex = j
+                        println("        endIndex: " + j)
+
+                        for (j <- i to 0 by -1) {
+                            println(s"        $i,   $j")
+                            println("        " + s(i) + " == " + s(j))
+
+                            if (s(j) == s(i)) {
+                                println("        dup found: " + s(j))
+                                startIndex = j
+                                println("        startIndex: " + j)
+
+                                var indexDiff = if (endIndex - 1 == startIndex) 0 else endIndex - startIndex
+                                println("        indexDiff: " + indexDiff)
+
+                                result = if (indexDiff > result) indexDiff else result
+
+                                break
+                            }
+                        }
+                    }
                 }
+
             }
         }
 
