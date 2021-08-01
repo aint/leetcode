bb767aba6a2988c2b03a947e4833724adb08480e
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2021-08-01 15:12:07 +0300

    Improve solution for Find All Anagram in String

--- string/FindAllAnagramsInString.go
+++ string/FindAllAnagramsInString.go
@@ -4,17 +4,13 @@ import (
 	"strings"
 )
 
-var p_map = make(map[rune]int, 0)
+var pMap map[rune]int
 
 func findAnagrams(s string, p string) []int {
 	// count chars in `p`
-	p_map = make(map[rune]int, 0)
+	pMap = make(map[rune]int, 0)
 	for _, r := range p {
-		if val, ok := p_map[r]; ok {
-			p_map[r] = val + 1
-		} else {
-			p_map[r] = 1
-		}
+		pMap[r] = pMap[r] + 1
 	}
 
 	pSize := len(p)
@@ -30,22 +26,22 @@ func findAnagrams(s string, p string) []int {
 }
 
 func isAnagram(s string, sIndex, pSize int) bool {
-	// copy p_map to a new [mutable] one
-	substr_map := make(map[rune]int, len(p_map))
-	for k, v := range p_map {
-		substr_map[k] = v
+	// copy pMap to a new [mutable] one
+	subStrMap := make(map[rune]int, len(pMap))
+	for k, v := range pMap {
+		subStrMap[k] = v
 	}
 
 	for i := sIndex; i < sIndex+pSize && i < len(s); i++ {
 		r := rune(s[i])
-		if val, ok := substr_map[r]; ok {
-			substr_map[r] = val - 1
+		if val, ok := subStrMap[r]; ok {
+			subStrMap[r] = val - 1
 		} else {
 			return false
 		}
 	}
 
-	for _, v := range substr_map {
+	for _, v := range subStrMap {
 		if v != 0 {
 			return false
 		}
